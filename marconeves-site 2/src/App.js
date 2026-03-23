174fbc75d8c85724f802431aa7a073321c2d2adf
marcopsneves-svg <marcopsneves@gmail.com>
2026-03-23 17:03:19 +0000

    Update App.js

--- marconeves-site 2/src/App.js
+++ marconeves-site 2/src/App.js
@@ -4,6 +4,9 @@ import HomePage from './pages/HomePage';
 import AvaliacaoPage from './pages/AvaliacaoPage';
 import ContactoPage from './pages/ContactoPage';
 import CartaoPage from './pages/CartaoPage';
+import ImoveisPage from './pages/ImoveisPage';
+import BlogPage from './pages/BlogPage';
+import ArtigoPage from './pages/ArtigoPage';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/avaliacao-gratuita" element={<AvaliacaoPage />} />
         <Route path="/contacto" element={<ContactoPage />} />
         <Route path="/cartao" element={<CartaoPage />} />
+        <Route path="/imoveis" element={<ImoveisPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<ArtigoPage />} />
       </Routes>
     </Router>
   );
