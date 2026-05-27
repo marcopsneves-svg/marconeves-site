fc59e89ff8593078dd477e1b9558f3ce247b43fd
marcopsneves-svg <marcopsneves@gmail.com>
2026-05-27 14:54:01 +0100

    Update VilaFrancaPage.js

--- marconeves-site 2/src/pages/VilaFrancaPage.js
+++ marconeves-site 2/src/pages/VilaFrancaPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-const FOTO = 'public/marco-camisa.jpg';
+const FOTO = '/images/marco-camisa.jpg';
 function FormAvaliacao({ concelho }) {
   const [form, setForm] = useState({ nome:'', telefone:'', email:'', morada:'', tipo:'Apartamento', mensagem:'' });
   const [enviado, setEnviado] = useState(false);
